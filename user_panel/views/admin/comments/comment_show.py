bea3ca863549bcedf55ef2b1007e644402ee1b61
Hadi <addii1385@gmail.com>
2024-05-23 13:02:37 +0330

    bug fixed

--- user_panel/views/admin/comments/comment_show.py
+++ user_panel/views/admin/comments/comment_show.py
@@ -5,4 +5,4 @@ def comment_show(request, id):
     c = get_object_or_404(Comment, id=id)
     c.checked = True
     c.save()
-    return redirect(reverse(''))
+    return redirect(reverse('blog_post_details', kwargs={'id': c.post.id})+f"#comment{id}")
